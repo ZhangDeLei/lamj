612008f3a5b9697fdf62ad81de9042b628e19358
ZhangDeLei <15156888276@139.com>
2018-05-22 15:02:36 +0800

    解决token验证bug

---
+++ src/main/java/com/coderfamily/lamj/common/filter/CorsFilter.java
@@ -0,0 +1,23 @@
+package com.coderfamily.lamj.common.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization,version,Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Allow-Methods");
+        filterChain.doFilter(request, response);
+    }
+}
